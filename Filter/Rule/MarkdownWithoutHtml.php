322384e1fa6eab392176e0f54edb010aaba65344
Eugene Myazin <meniam@gmail.com>
2019-05-11 19:44:21 +0300

    Markdown Filters

---
+++ Filter/Rule/MarkdownWithoutHtml.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Meniam\Bundle\CoreBundle\Filter\Rule;
+
+use League\CommonMark\CommonMarkConverter;
+use Meniam\Bundle\CoreBundle\Filter\FilterRule;
+
+class MarkdownWithoutHtml extends FilterRule
+{
+    /** @var CommonMarkConverter */
+    private $markdownParser;
+
+    public function filter($value)
+    {
+        return $this->getMarkdownParser()->convertToHtml($value);
+    }
+
+    /**
+     * @return CommonMarkConverter
+     */
+    private function getMarkdownParser()
+    {
+        if (!$this->markdownParser) {
+            $this->markdownParser = new CommonMarkConverter([
+                'renderer' => [
+                    'block_separator' => "\n",
+                    'inner_separator' => "\n",
+                    'soft_break'      => "\n",
+                ],
+                'enable_em' => true,
+                'enable_emphasis' => true,
+                'enable_strong' => true,
+                'use_asterisk' => true,
+                'use_underscore' => true,
+                'html_input' => 'strip',
+                'allow_unsafe_links' => false,
+            ]);
+        }
+
+        return $this->markdownParser;
+    }
+}
