0baeb1864cfce2b4f5ce809048b9d2d00477a273
Eugene Myazin <meniam@gmail.com>
2019-05-12 23:42:37 +0300

    urlSaveGet Optimize

--- Controller/AbstractCoreController.php
+++ Controller/AbstractCoreController.php
@@ -20,6 +20,7 @@ use Meniam\Bundle\CoreBundle\Traits\ValidatorTrait;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Router;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 abstract class AbstractCoreController extends AbstractController
@@ -65,6 +66,11 @@ abstract class AbstractCoreController extends AbstractController
     protected function addFlashTrans(string $type, string $message, $params = null)
     {
         $this->addFlash($type, $this->trans($message, $params));
+    }
+
+    protected function urlSaveGet($replace = array(), $delete = array(), $route = null, $parameters = array(), $referenceType = Router::ABSOLUTE_PATH)
+    {
+        return $this->getRequestService()->urlSaveGet($replace, $delete, $route, $parameters, $referenceType);
     }
 
     /**
