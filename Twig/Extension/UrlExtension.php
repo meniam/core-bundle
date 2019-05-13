7d505f60a337536bdec1280f5f219d6948ad1ca7
Eugene Myazin <meniam@gmail.com>
2019-05-13 18:57:06 +0300

    UrlService

--- Twig/Extension/UrlExtension.php
+++ Twig/Extension/UrlExtension.php
@@ -3,32 +3,33 @@
 namespace Meniam\Bundle\CoreBundle\Twig\Extension;
 
 use Meniam\Bundle\CoreBundle\Service\RequestService;
-use Meniam\Bundle\CoreBundle\Traits\ServiceSystemTrait;
 use Symfony\Component\Routing\Router;
+use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
-class UrlExtension extends AbstractServiceSubscriberExtension
+class UrlExtension extends AbstractExtension
 {
-    use ServiceSystemTrait;
+    /**
+     * @var RequestService
+     */
+    private $requestService;
 
-    public static function getSubscribedServices()
+    public function __construct(RequestService $requestService)
     {
-        return array_merge(parent::getSubscribedServices(), [
-            RequestService::class
-        ]);
+        $this->requestService = $requestService;
     }
 
     public function getFunctions()
     {
-        return array(
+        return [
             new TwigFunction('url_save_get', [$this, 'urlSaveGetFilter'], ['is_safe' => ['all']]),
             new TwigFunction('url_domain', [$this, 'urlDomainFilter'], ['is_safe' => ['all']]),
-        );
+        ];
     }
 
     public function urlSaveGetFilter($replace = array(), $delete = array(), $route = null, $parameters = array(), $referenceType = Router::ABSOLUTE_PATH)
     {
-        return $this->getRequestService()->urlSaveGet($replace, $delete, $route, $parameters, $referenceType);
+        return $this->requestService->urlSaveGet($replace, $delete, $route, $parameters, $referenceType);
     }
 
     public function urlDomainFilter($url)
