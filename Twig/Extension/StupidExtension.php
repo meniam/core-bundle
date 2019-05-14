c579ea1aecd261aed8846baf09d596b7d70697fa
Eugene Myazin <meniam@gmail.com>
2019-05-14 18:17:31 +0300

    Small Fixes

--- Twig/Extension/StupidExtension.php
+++ Twig/Extension/StupidExtension.php
@@ -2,6 +2,7 @@
 
 namespace Meniam\Bundle\CoreBundle\Twig\Extension;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Meniam\Bundle\CoreBundle\Twig\TokenParser\MarkdownTokenParser;
 use Meniam\Bundle\CoreBundle\Filter\FilterStatic;
 use Meniam\Bundle\CoreBundle\Filter\Rule\MarkdownWithHtml;
@@ -23,7 +24,6 @@ class StupidExtension extends AbstractExtension
      */
     private $storage = [];
 
-
     public function getFilters()
     {
         return [
@@ -39,7 +39,7 @@ class StupidExtension extends AbstractExtension
             new TwigFilter('ends_with', [StringUtil::class, 'endsWith']),
             new TwigFilter('starts_with', [StringUtil::class, 'startsWith']),
 
-            new TwigFilter('typo', [$this, 'typo']),
+            new TwigFilter('typo',        [$this, 'typo']),
 
             new TwigFilter('truncate', [StringUtil::class, 'safeTruncate']),
             new TwigFilter('truncate_html', [StringUtil::class, 'safeTruncateHtml']),
@@ -51,12 +51,14 @@ class StupidExtension extends AbstractExtension
             new TwigFilter('array_unique', 'array_unique'),
 
             # Hash and decodes
-            new TwigFilter('sha1',          [$this, 'sha1Filter']),
-            new TwigFilter('md5',           [$this, 'md5Filter']),
-            new TwigFilter('base64_encode', [$this, 'base64EncodeFilter']),
-            new TwigFilter('base64_decode', [$this, 'base64DecodeFilter']),
-            new TwigFilter('yaml_encode',   [$this, 'yamlEncodeFilter']),
-            new TwigFilter('yaml_decode',   [$this, 'yamlDecodeFilter']),
+            new TwigFilter('sha1',          [$this, 'sha1']),
+            new TwigFilter('md5',           [$this, 'md5']),
+            new TwigFilter('base64_encode', [$this, 'base64Encode']),
+            new TwigFilter('base64_decode', [$this, 'base64Decode']),
+            new TwigFilter('yaml_encode',   [$this, 'yamlEncode']),
+            new TwigFilter('yaml_decode',   [$this, 'yamlDecode']),
+            new TwigFunction('json_decode', [$this, 'jsonDecode']),
+            new TwigFunction('json_encode', [$this, 'jsonEncode']),
 
             # Pretty
             new TwigFilter('file_pretty_size', array($this, 'filePrettySizeFilter')),
@@ -64,10 +66,10 @@ class StupidExtension extends AbstractExtension
 
             // Casts
             new TwigFilter('string', [$this, 'stringFilter']),
-            new TwigFilter('int', [$this, 'intFilter']),
-            new TwigFilter('bool', [$this, 'boolFilter']),
-            new TwigFilter('float', [$this, 'floatFilter']),
-            new TwigFilter('array', [$this, 'arrayFilter']),
+            new TwigFilter('int',    [$this, 'intFilter']),
+            new TwigFilter('bool',   [$this, 'boolFilter']),
+            new TwigFilter('float',  [$this, 'floatFilter']),
+            new TwigFilter('array',  [$this, 'arrayFilter']),
 
             // System
             new TwigFilter('basename', 'basename'),
@@ -78,28 +80,37 @@ class StupidExtension extends AbstractExtension
 
     public function getFunctions()
     {
-        return array(
-            # markdown
-            new TwigFunction('markdown', array($this, 'markdown'), ['is_safe' => ['all']]),
-
-            new TwigFilter('typo', [$this, 'typo']),
+        return [
+            new TwigFunction('markdown', [$this, 'markdown'], ['is_safe' => ['all']]),
+            new TwigFunction('typo',     [$this, 'typo']),
 
             # storage
-            new TwigFunction('put_to_storage', array($this, 'putToStorage'), ['is_safe' => ['all']]),
-            new TwigFunction('get_from_storage', array($this, 'getFromStorage'), ['is_safe' => ['all']]),
+            new TwigFunction('put_to_storage',   [$this, 'putToStorage'], ['is_safe' => ['all']]),
+            new TwigFunction('get_from_storage', [$this, 'getFromStorage'], ['is_safe' => ['all']]),
 
-            # path, url
-            new TwigFunction('pathStarts', array($this, 'pathStarts'), ['is_safe' => ['all']]),
-            new TwigFunction('pathEquals', array($this, 'pathEquals'), ['is_safe' => ['all']]),
-
-            new TwigFunction('spacer', array($this, 'spacer'), ['is_safe' => ['all']]),
+            new TwigFunction('spacer',           [$this, 'spacer'], ['is_safe' => ['all']]),
 
             # System
             new TwigFunction('array_key_exists', 'array_key_exists'),
-            new TwigFunction('array_unique', 'array_unique'),
-            new TwigFunction('print_r', 'print_r'),
-            new TwigFunction('range', 'range'),
-            new TwigFunction('pathinfo', 'pathinfo'),
+            new TwigFunction('array_unique',     'array_unique'),
+            new TwigFunction('print_r',          'print_r'),
+            new TwigFunction('range',            'range'),
+            new TwigFunction('pathinfo',         'pathinfo'),
+
+            new TwigFunction('is_ajax_request', [$this, 'isAjaxRequest']),
+            new TwigFunction('array_intersect', [$this, 'arrayIntersect']),
+
+            new TwigFunction('get_cookie', [$this, 'getCookie']),
+
+            # Hash and decodes
+            new TwigFunction('sha1',          [$this, 'sha1']),
+            new TwigFunction('md5',           [$this, 'md5']),
+            new TwigFunction('base64_encode', [$this, 'base64Encode']),
+            new TwigFunction('base64_decode', [$this, 'base64Decode']),
+            new TwigFunction('yaml_encode',   [$this, 'yamlEncode']),
+            new TwigFunction('yaml_decode',   [$this, 'yamlDecode']),
+            new TwigFunction('json_decode',   [$this, 'jsonDecode']),
+            new TwigFunction('json_encode',   [$this, 'jsonEncode']),
 
             // Casts
             new TwigFunction('string', [$this, 'stringFilter']),
@@ -107,7 +118,7 @@ class StupidExtension extends AbstractExtension
             new TwigFunction('bool',   [$this, 'boolFilter']),
             new TwigFunction('float',  [$this, 'floatFilter']),
             new TwigFunction('array',  [$this, 'arrayFilter']),
-        );
+        ];
     }
 
     /**
@@ -123,16 +134,15 @@ class StupidExtension extends AbstractExtension
         ];
     }
 
-    public function yamlEncodeFilter($data, $inline = 10)
+    public function yamlEncode($data, $inline = 10)
     {
         return YamlUtil::encode($data, $inline);
     }
 
-    public function yamlDecodeFilter($data)
+    public function yamlDecode($data)
     {
         return YamlUtil::decode($data);
     }
-
 
     public function markdown($text)
     {
@@ -185,22 +195,22 @@ class StupidExtension extends AbstractExtension
         return (array) $input;
     }
 
-    public function sha1Filter($str)
+    public function sha1($str)
     {
         return sha1((string)$str);
     }
 
-    public function md5Filter($str)
+    public function md5($str)
     {
         return md5((string)$str);
     }
 
-    public function base64EncodeFilter($str)
+    public function base64Encode($str)
     {
         return base64_encode((string)$str);
     }
 
-    public function base64DecodeFilter($str)
+    public function base64Decode($str)
     {
         return base64_decode((string)$str);
     }
@@ -245,19 +255,49 @@ class StupidExtension extends AbstractExtension
         return FilterStatic::filterValue($str, Typographics::class);
     }
 
-    /**
-     * Filters field name by changing dot notation into array notation.
-     * @param  string $str
-     * @return string
-     */
+    public function isAjaxRequest()
+    {
+        return (
+            !empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest');
+    }
+
     public function fieldNameFilter($str)
     {
         $path = explode('.', rtrim($str, '.'));
         return array_shift($path) . ($path ? '[' . implode('][', $path) . ']' : '');
     }
 
-    public function getName()
+    public function getCookie($key)
     {
-        return 'stupid_extension';
+        return filter_input(INPUT_COOKIE, $key, FILTER_SANITIZE_STRING);
+    }
+
+    public function jsonDecode($str, $assoc = false, $depth = 512, $options = 0)
+    {
+        return json_decode(html_entity_decode($str), $assoc, $depth, $options);
+    }
+
+    public function jsonEncode($str)
+    {
+        return json_encode($str, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Wrapper for array_intersect() method
+     *
+     * @param array $array1
+     * @param array $array2
+     * @return array|ArrayCollection
+     */
+    public function arrayIntersect($array1, $array2)
+    {
+        if ($array1 instanceof ArrayCollection && $array2 instanceof ArrayCollection) {
+            return new ArrayCollection(
+                array_merge($array1->toArray(), $array1->toArray())
+            );
+        }
+
+        return array_intersect($array1, $array2);
     }
 }
