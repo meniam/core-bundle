07d0bbf0635e42ef4f8f440be46665a030da45ce
Eugene Myazin <meniam@gmail.com>
2019-05-28 12:40:48 +0300

    User

--- Entity/User.php
+++ Entity/User.php
@@ -12,4 +12,21 @@ abstract class User implements Serializable, UserInterface
     const ROLE_ADMIN         = 'ROLE_ADMIN';       // Администратор обычный, права минимальные
     const ROLE_EMPLOYEE      = 'ROLE_EMPLOYEE';    // Роль сотрудника без доступа в админку
     const ROLE_USER          = 'ROLE_USER';        // Пользователь сайта
+
+    abstract function getRole();
+
+    public function isRoot()
+    {
+        return ($this->getRole() == self::ROLE_ROOT);
+    }
+
+    public function isAdmin()
+    {
+        return in_array((string)$this->getRole(), [self::ROLE_ROOT, self::ROLE_SUPER_ADMIN, self::ROLE_ADMIN]);
+    }
+
+    public function isEmployee()
+    {
+        return ($this->getRole() == self::ROLE_EMPLOYEE) || $this->isAdmin();
+    }
 }
